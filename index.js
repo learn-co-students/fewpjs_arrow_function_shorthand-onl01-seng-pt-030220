6f9a17acefcd7376e8dda9cedb23bee03cc5abe7
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-10-04 15:29:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
 
-let divide = () => {
-  return 2000 / 100
-}
+let divide = () =>  2000 / 100;
 
-let square = num => num * num
+let square = num => num * num;
 
-let add = (num1, num2) => { return num1 + num2 }
+let add = (num1, num2) => num1 + num2;
